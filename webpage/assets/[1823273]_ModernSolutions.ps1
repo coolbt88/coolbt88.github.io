9219537ab563513ee97e84550a9a0d63770b6ac4
coolbt88 <braedenbt88@gmail.com>
2020-03-25 19:20:04 -0400

    download

---
+++ webpage/assets/[1823273]_ModernSolutions.ps1
@@ -0,0 +1,15 @@
+﻿Fu nction KillThatProcess([string]$name){
+$Process = Get-Process $name
+$prognum = $process.count
+$choice = Read-Host "Total number of $name open: $prognum. Terminate? Y/N"
+if($choice -eq "y" -or $choice  -eq "Y"){$Process | Stop-Process}
+else{Write-Host -BackgroundColor red -ForegroundColor white "Nothing has been done"}
+}
+
+Function Bamboozle([string]$Path = "."){
+$char = (97..122) | Get-Random | %{[char]$_}
+Write-Host -BackgroundColor Yellow -ForegroundColor black "I am wanted for manslaughter in 3 different states. Also I chose the letter $char"
+$files = Get-ChildItem $Path -include *"$char" -Recurse | Where-Object { $Path.Attributes -ne "Directory"}
+if($files -eq $null){Write-Host -BackgroundColor red -ForegroundColor white "There are no files"}
+else{$files | Remove-Item -WhatIf}
+}
